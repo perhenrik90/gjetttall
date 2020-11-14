ee50d8ffce1ac32216672d53253ba248b7236b14
Per-Henrik Kvalnes <perhenrik.kvalnes@gmail.com>
2020-11-14 11:31:14 +0100

    lagt til en fungerende versjon av 'gett et tall' programmet på Norsk

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,30 @@ use rand::Rng;
 
 
 fn main() {
-    println!("Gjett et tall!");
+    println!("Gjett et tall mellom 1 og 10!");
 
-    println!("Hva er det du gjetter?");
+    println!("Hva gjetter du?");
 
-    let mut gjettet = String::new();
+    // Lag hemmelig tall
     let hemmelighet = rand::thread_rng().gen_range(1, 10);
-    
+
+    // Les inn tall
+    let mut gjettet_str = String::new();
+
+
     io::stdin()
-	.read_line(&mut gjettet)
+	.read_line(&mut gjettet_str)
 	.expect("Feilet aa lese linja!");
 
+    let gjettet: i32 = gjettet_str.trim().parse().expect("Kunne ikke tolke tall");
+
+    if hemmelighet == gjettet {
+	    println!("Bra. Du gjettet riktig! Tallet var {}", gjettet_str);
+    }
+    else{
+	println!("Sorry, du gjettet feil.")
+    }
+    
     println!("Du gjettet: {}", gjettet);
+    println!("Hemmelig tall var: {}", hemmelighet);
 }
